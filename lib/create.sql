f159d7ebfa4c9163f7440c598e287f7d6f7265e5
Tong Tang <pichip22@hotmail.com>
2023-08-01 14:19:29 -0400

    Completed

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+    id INTEGERPRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES
+
+("Mr.Chocolate",1,"M","Red","Mad",True),
+("Rowdy",2,"M","Blue","Shy",True),
+("Tabitha",3,"F","Black","Outgoing",True),
+("Sergent Brown",10,"M", "Brown","Human", False),
+("Melissa",4,"F","Gray","Aggressive",True),
+("Grinch",5,"M","Green","Festive", False),
+("Wendy",6,"F","Orange","Fast",True),
+(NULL,1,"F","Black","Nonexist",False);
